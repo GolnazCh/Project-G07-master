a412da9c7274f2fc61aebf0318ba14b9e7e4f1bf
Golnaz Chehrazi <105564696+GolnazCh@users.noreply.github.com>
2023-11-12 23:21:25 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/project_g07/activity/LessonListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project_g07.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.project_g07.R
+
+class LessonListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lesson_list)
+    }
+}
---
+++ app/src/main/java/com/example/project_g07/activity/WelcomeBackActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project_g07.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.project_g07.R
+
+class WelcomeBackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_back)
+    }
+}
